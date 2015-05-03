96d7efd6c8717bcb9e8065c80ff94ff146c43647
Tanguy Hélesbeux <tanguy.helesbeux@gmail.com>
2015-05-03 16:04:01 +0200

    recursive image reader

--- app/ios-lab/ios-lab/ViewController.m
+++ app/ios-lab/ios-lab/ViewController.m
@@ -10,6 +10,8 @@
 
 @interface ViewController ()
 
+@property (nonatomic, strong) UIImageView *imageView;
+
 @end
 
 @implementation ViewController
@@ -19,8 +21,57 @@
     // Do any additional setup after loading the view, typically from a nib.
     
     self.view.backgroundColor = [UIColor redColor];
-    
+}
 
+- (void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
+    
+    [self readFiles];
+}
+
+- (UIImageView *)imageView {
+    if (!_imageView) {
+        _imageView = [[UIImageView alloc] initWithFrame:self.view.bounds];
+        [self.view addSubview:_imageView];
+    }
+    return _imageView;
+}
+
+- (void)setImage:(UIImage *)image {
+    self.imageView.image = image;
+}
+
+- (BOOL)isDirectory:(NSString *)path {
+    BOOL isDirectory = NO;
+    [[NSFileManager defaultManager] fileExistsAtPath:path isDirectory:&isDirectory];
+    return isDirectory;
+}
+
+- (NSArray *)readImagesAtPath:(NSString *)path {
+    NSError *error;
+    NSArray *files = [[NSFileManager defaultManager] contentsOfDirectoryAtPath:path error:&error];
+    if (files == nil) {
+        NSLog(@"Error reading contents of documents directory: %@", [error localizedDescription]);
+    }
+    
+    NSMutableArray *images = [[NSMutableArray alloc] init];
+    NSString *fullPath = nil;
+    NSLog(@"%@", path);
+    for (NSString *file in files) {
+        fullPath = [path stringByAppendingPathComponent:file];
+        UIImage *image = [UIImage imageWithContentsOfFile:fullPath];
+        if (image) {
+            NSLog(@"%@", file);
+            [images addObject:image];
+        } else if ([self isDirectory:fullPath]) {
+            NSArray *subImages = [self readImagesAtPath:fullPath];
+            [images addObject:subImages];
+        } else {
+            NSLog(@"Failed to read : %@", file);
+        }
+    }
+    
+    return images;
 }
 
 // Source: http://www.raywenderlich.com/1948/itunes-tutorial-for-ios-how-to-integrate-itunes-file-sharing-with-your-ios-app
@@ -28,18 +79,7 @@
     NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
     NSString *publicDocumentsDir = [paths objectAtIndex:0];
     
-    NSError *error;
-    NSArray *files = [[NSFileManager defaultManager] contentsOfDirectoryAtPath:publicDocumentsDir error:&error];
-    if (files == nil) {
-        NSLog(@"Error reading contents of documents directory: %@", [error localizedDescription]);
-    }
-    
-    for (NSString *file in files) {
-        if ([file.pathExtension compare:@"sbz" options:NSCaseInsensitiveSearch] == NSOrderedSame) {
-            NSString *fullPath = [publicDocumentsDir stringByAppendingPathComponent:file];
-            //            [retval addObject:fullPath];
-        }
-    }
+    NSArray *images = [self readImagesAtPath:publicDocumentsDir];
 }
 
 @end
